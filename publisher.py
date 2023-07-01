6fb68d3489d9422dda96f66da5b35dafac8677c7
santos-bruna <70614304+santos-bruna@users.noreply.github.com>
2023-07-01 16:08:14 -0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -1,11 +1,18 @@
 import zmq, time
 from constPS import * #-
 
+
 context = zmq.Context()
-s = context.socket(zmq.PUB)        # create a publisher socket
-p = "tcp://"+ HOST +":"+ PORT      # how and where to communicate
-s.bind(p)                          # bind socket to the address
-while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+socket = context.socket(zmq.PUB)  # Cria um socket de publicação
+
+def send_message(topic, message):
+    socket.send_multipart([bytes(topic, 'utf-8'), bytes(message, 'utf-8')])
+
+# Conecta ao servidor
+socket.connect("tcp://localhost:5555")
+
+# Exemplos de envio de mensagens
+send_message("user123", "Olá, usuário 123!")
+send_message("topic1", "Esta é uma mensagem para o tópico 1.")
+send_message("topic2", "Esta é uma mensagem para o tópico 2.")
+
